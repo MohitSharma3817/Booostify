4fae6ad8fe5bd7c59a07fb70165749f6c21c5d35
unknown <sharmamohit3817@gmail.com>
2026-01-20 12:20:34 +0530

    updated

--- src/data/productData.js
+++ src/data/productData.js
@@ -1,45 +1,50 @@
 import RJLINK from "../assets/ProductImages/RJLINK.jpg";
+import CELLEXXA from "../assets/ProductImages/CELLEXXA.jpg";
+import Quadband from "../assets/ProductImages/quad-band-mobile-signal-booster.jpg";
+import triband from "../assets/ProductImages/5-G.jpeg";
+import fifth from "../assets/ProductImages/fifth.jpg";
+import last from "../assets/ProductImages/last.jpg";
 const productsData = [
   {
     id: 1,
-    title: "RJLINK Signal Booster ",
-    description: "RJLINK JIO/2G/3G/4G Router mobile phone signal booster all sim working boost network & data complete kit area coverage 3500 sq.feet GST included",
+    title: "Signal Booster ",
+    description: "JIO/2G/3G/4G Router mobile phone signal booster all sim working boost network & data complete kit area coverage 3500 sq.feet GST included",
     image: RJLINK,
     
   },
   {
     id: 2,
-    title: "CELLEXXA 5G Signal Booster",
-    description: "CELLEXXA 5G Signal Booster for Home, Boosts Network & Data, Complete Kit, Area Coverage 4500 Sq.Feet",
-    image: "src\\data\\ProductImages\\5g-network-booster.jpg",
+    title: "5G Signal Booster",
+    description: "5G Signal Booster for Home, Boosts Network & Data, Complete Kit, Area Coverage 4500 Sq.Feet",
+    image: CELLEXXA,
     
   },
   {
     id: 3,
-    title: "Industrial Booster",
-    description: "High power solutions for industries",
-    image: "https://lintratek.in/wp-content/uploads/2023/11/sasa-min.png",
+    title: "2G/3G/4G/5G Quad Band Signal Booster",
+    description: "2G/3G/4G/5G Quad Band Signal Booster All Sim Supported Booster Network Boosts & Data Also Complete Kit Area Coverage 4500Sq.Feet",
+    image: Quadband,
     
   },
   {
     id: 4,
-    title: "Home Booster ",
-    description: "Best for houses and small apartments",
-    image: "https://lintratek.in/wp-content/uploads/2023/11/sasa-min.png",
+    title: "3G/4G/5G triband Router ",
+    description: "3G/4G/5G triband Router mobile phone signal booster all sim working boost network & data complete kit area coverage 3500 sq.feet gst included",
+    image: triband,
     
   },
   {
     id: 5,
-    title: "Office Booster",
-    description: "Ideal for offices and commercial buildings",
-    image: "https://lintratek.in/wp-content/uploads/2023/11/sasa-min.png",
+    title: "Single Band Booster",
+    description: "Jio Single Band Mobile Phone Signal Booster",
+    image: fifth,
     
   },
   {
     id: 6,
-    title: "Industrial Booster",
-    description: "High power solutions for industries",
-    image: "https://lintratek.in/wp-content/uploads/2023/11/sasa-min.png",
+    title: "Triband mobile phone network booster",
+    description: "jio/2G/3G/4G triband mobile phone network booster & data complete kit area coverage 4500 sq.feet GST included",
+    image: last,
     
   },
 ]
